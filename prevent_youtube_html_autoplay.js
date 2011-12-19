c84052daac13019add9df1e4975cd62af244d53a
Christian Axelsson <christian@smiler.se>
2011-12-19 15:21:06 +0100

    Initial commit

---
+++ prevent_youtube_html_autoplay.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name           Youtube HTML5 stop autoplay
+// @namespace      http://smiler.se/
+// @description    Prevent HTML5 videos on youtube from autoplaying
+// @include        http://youtube.com/watch?*
+// @include        http://*.youtube.com/watch?*
+// @include        http://youtube.com/watch#*
+// @include        http://*.youtube.com/watch#*
+// @include        http://*.youtube.com/embed/*
+// @include        http://youtube.com/embed/*
+// ==/UserScript==
+
+function onLoad() {
+    video = document.getElementsByTagName('video')[0];
+    if (video) {
+        video.pause();
+        video.currentTime = 0;
+    }
+}
+
+window.addEventListener('load', onLoad, false);
